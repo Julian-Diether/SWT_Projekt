e76ecd531464a3133068bb93a309e7d5f323452f
Julian-Diether <julian.diether@icloud,com>
2023-05-17 10:47:50 +0200

    fehler, nichts funktioniert mehr :,(

--- main.cpp
+++ main.cpp
@@ -1,39 +1,45 @@
 //Software Technik Projekt Gruppe1
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char myinput [15];
-    printf("************************\n");
-    printf("Willkommen zum Musikplayer!\n");
+    char myinput;
+    /*printf("************************\n");
+    printf("Willkommen zum Musikplayer!\n");*/
     printf("Moechten sie eine Musik-Playlist initialisieren? (Ja/Nein): ");
-    scanf("%s", myinput);
-    printf("%s", myinput); //testtest
-    printf("\n");
+    scanf("%c", myinput);
+    printf("%c", myinput); //testtest
     
-    if(myinput=="Ja" || myinput=="ja" || myinput=="JA") {
-        //bibliothek initialisieren
-        printf("Geben sie den Namen der zu initialisierenden Playlist ein:\n"); // sprache zu kompliziert fuer den benutzer??
 
-    } else if(myinput=="Nein" || myinput=="nein" || myinput=="NEIN") {
+    if(myinput=='ja') {
+        //bibliothek initialisieren
+        printf("\nGeben sie den Namen der zu initialisierenden Playlist ein:\n"); // sprache zu kompliziert fuer den benutzer??
+
+    } else {
+        printf("\nfehler");
+    } 
+    /*else if(myinput=="Nein" || myinput=="nein" || myinput=="NEIN") {
         //bibliothek NICHT initialisieren
         //hier soll es normal mit dem musikplayer weitergehen
 
         printf("test - fall: nein");
     } else {
-        //falsche eingabe (nicht 'ja' oder 'nein') --> wiederholen
+        //falsche eingabe (nicht "ja" oder "nein") --> wiederholen
+        printf("hdahsfhdfsdo\n");
         printf("Moechten sie eine Musik-Playlist initialisieren? (Ja/Nein):");
         scanf("%s", myinput);
-    }
+    }*/
 
 
 
     return 0;
 }
 
+/*/
 bool myinitialize(char *pointerFile) {
     FILE *initFile;
     //Bibiliothek initialisieren und status(erfolg/fehler) zurueckgeben!
     //Eingang: Pointer auf Datei??
     initFile = fopen(pointerFile, "r");
 
-}
+}*/
