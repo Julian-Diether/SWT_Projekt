924b2b2871f0aad0b775caff3b5e8de1aba48f73
Julian-Diether <julian.diether@icloud,com>
2023-05-18 13:28:13 +0200

    initialisieren halb fertig #2

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ SoftwareTechnik Projekt
     hinzufuegen anderer teilnehmer
     
 -erste Test in cpp
+    eingaben auslesen(nicht case-sensitiv), verschiedene programmebenen erstellen
+
+**18.05.**?
+json datei layout:
+songtitel, kuenstler, album, jahr, laenge des titels?, genre, explicit(y/n)?
+
--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 //Software Technik Projekt Gruppe1
+//inkludieren saemtlicher bibliotheken
 #include <stdio.h>
 #include <string> //??!?
 #include <string.h>
-#include <iostream>
+#include <iostream> //brauchen wir das alles? macht es das programm zu langsam/grosss?
+#include <fstream>
+#include "nlohmann/json.hpp"//nlohmann json bibliothek
+using json = nlohmann::json;
+//cpp sachen??!
 using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
+//initialisierung eigener funktionen
+void myinitialize();
+void myopenfile();
+//deklarierung globaler variablen
+const char* mynein = "nein";
+const char* myja = "ja";
+
 
 int main() {
+    /*std::string answer{};
+    std::string mynein = "nein";
+    std::string myja = "ja";*/
+
+    myinitialize();
+    
+
+    return 0;
+}
+
+
+void myinitialize() {
     char answer[20];
-    //std::string answer{};
     bool repeat = true;
-    const char* mynein = "nein";
-    const char* myja = "ja";
-    //std::string mynein = "nein";
-    //std::string myja = "ja";
-
-
+    
     while (repeat) {
         cout << "\nMoechten sie eine Musik-Playlist initialisieren? (Ja/Nein): ";
         cin >> answer;
@@ -26,9 +44,9 @@ int main() {
             if (strcasecmp(answer, myja) == 0) {//if (answer == "ja") {
             repeat = false;
             //bibliothek initialisieren
-            cout << "\nGeben sie den Namen der zu initialisierenden Playlist ein:\n" << endl; // sprache zu kompliziert fuer den benutzer??
-            
-            cout << "answer=ja" << endl; //testtest
+            myopenfile();
+            //cout << "\nGeben sie den Namen der zu initialisierenden Playlist ein:\n" << endl; // sprache zu kompliziert fuer den benutzer??
+            //cout << "answer=ja" << endl; //testtest
             
         } if (strcasecmp(answer, mynein) == 0) {
             repeat = false;   
@@ -38,9 +56,26 @@ int main() {
             cout << "answer=nein" << endl; //testtesttest
         } /*else {  //nichts es geht von selbst weiter  }*/
     }
-    return 0;
 }
 
+
+void myopenfile() {
+    cout << "\nGeben sie den Namen der zu oeffnenden Playlist ein:\n" << endl;
+    std::ifstream file("testtesttest.json");
+    //oeffnen derr .json datei^^
+    if (!file.is_open()) {
+        std::cerr << "Die .json Datei konnte nicht geoeffnet werden!" << std::endl;
+    }
+
+    nlohmann::json jsonContent;
+    file >> jsonContent;
+    //cout << "funktioniert das?";
+    
+    //jsonContent[0][]?
+    //wie genau benenne ich die dateitypen?? mit namen oder dateitypen? muss ich das ganze in eine ober kategorie??
+    //wie suche ich spaeter die einzelnen metadaten?? muss ich die werte fuer die liste 'sterilisieren' um probleme zu verhindern?
+
+}
 
     /*    printf("************************\n");
     printf("Willkommen zum Musikplayer!\n");
@@ -81,3 +116,4 @@ bool myinitialize(char *pointerFile) {
     initFile = fopen(pointerFile, "r");
 
 }*/
+
