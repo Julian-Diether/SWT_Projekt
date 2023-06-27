a3820df5ff2d36f7c4182ea1b502c64217569358
Julian-Diether <julian.diether@icloud,com>
2023-06-27 16:24:50 +0200

    probleme mit git behoben

---
+++ test-cases/SWT_Projekt-tests/main.cpp
@@ -0,0 +1,9 @@
+//Software Technik Projekt Gruppe1 -- Julian Diether, Lionel Felipe, Noah Traub, Frederik Kempke
+#include "fileops.cpp"//
+#include "menu.cpp"
+
+int main() {
+    myinitialize();//wenn ja wird initialisiert/wenn nein --> main_menu();
+    return 0;
+}
+
